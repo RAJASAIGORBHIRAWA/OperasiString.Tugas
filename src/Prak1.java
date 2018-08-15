3208f6b2e86f56ef7b589f150766b47e5ffebced
ASUS <ASUS@DESKTOP-9FMG8PT>
2018-08-15 12:07:10 -0700

    Initial Commit

---
+++ src/Prak1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Prak1 {
+    public static void main(String[] args) 
+    {
+        String identitas = "Rajasa Igor B. / X-RPL 3 / 33";
+        System.out.println("Identitas: " + identitas);
+   
+    }
+}
