af8e81207f533fa6f3b5e8ebde34d567a68de92a
ASUS <ASUS@DESKTOP-9FMG8PT>
2018-08-15 08:22:12 -0700

    Initial Commit

---
+++ src/praktek02.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+public class praktek02 {
+    public static void tampilJudul(String identitas) 
+    {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (vokal ke Angka)\n");
+    }
+    
+    public static void main(String[] args)
+    {
+     String identitas = "Rajasa Igor Bhirawa / X-RPL 3 / 33";
+    
+     tampilJudul(identitas);   
+    }           
+    
+}
