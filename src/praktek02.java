0ec479f9698f6bbcd1bbfb873dd2047972c261f3
ASUS <ASUS@DESKTOP-9FMG8PT>
2018-08-16 15:19:28 -0700

    Tambah Konversi

--- src/praktek02.java
+++ src/praktek02.java
@@ -20,7 +20,21 @@ public class praktek02 {
         System.out.println("Kalimat Asli : " + kalimat);
         
         return kalimat;
-    }        
+    }   
+    
+    private static String vocal2Angka(String kalimat)
+    {
+        char [][] arConvert = 
+        {{'a', '4',},{'i', 'i'},{'u', '2'},{'e', '3'},{'o', 'o'}};
+        
+        kalimat = kalimat.toLowerCase();
+        for(int i = 0; i<arConvert.length; i++)
+            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+        
+        return kalimat;
+    }
+            
+            
     public static void main(String[] args)
     {
      String identitas = "Rajasa Igor Bhirawa / X-RPL 3 / 33";
@@ -28,6 +42,10 @@ public class praktek02 {
      tampilJudul(identitas);   
      
      String kalimat = tampilInput();
+     
+     String convert = vocal2Angka(kalimat);
+     
+     
     }           
     
     
